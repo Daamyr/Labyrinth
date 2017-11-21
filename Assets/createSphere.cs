04c96459a958f7a040ebcbe9b2d64997c332180c
Daamyr <maxime.damour974@gmail.com>
2017-11-20 19:27:46 -0500

    First commit

---
+++ Assets/createSphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class createSphere : MonoBehaviour {
+
+	public Button yourButton;
+	public GameObject shit;
+	public Vector3 position;
+	public Quaternion rotation;
+	public int couillon;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		//GameObject clone = Instantiate (shit, position, rotation);
+		for (int i = 0; i < couillon; i++) {
+			
+			Instantiate (shit, position, rotation);
+		}
+	}	
+}
