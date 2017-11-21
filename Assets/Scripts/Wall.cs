cc9df461a403f951621e6e4fa49832e57da738cf
Maxime Damour <mdamour@cognibox.com>
2017-11-21 01:37:01 -0500

    Jsais pas ce que je fais # 1

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall /*: MonoBehaviour*/ {
+
+	//cell
+	enum FACING { NORTH, EAST, SOUTH, WEST };
+
+	//WallTexture texture;
+	Texture2D texture;
+
+	public Wall(){
+		GraphicManager graphicManager = GraphicManager.getInstance ();
+		texture = graphicManager.LoadTexture ("wall");
+		Debug.Log ("\tWall created with texture " + texture);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
